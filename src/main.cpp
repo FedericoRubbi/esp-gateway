a474188372e8eb866ed7dbf55eb76ec0caa2b27d
federicorubbi <federico.rubbi@yandex.com>
2024-02-26 16:19:16 +0100

    add endpoint

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,27 @@
 #include <ESP8266React.h>
-#include <LightMqttSettingsService.h>
-#include <LightStateService.h>
+#include <RestClient.h>
+#include <Sensor.h>
 
 #define SERIAL_BAUD_RATE 115200
+#define SERVER_PORT 80
+#define HOST_ADDRESS "nodemcu.cliquesrl.it"
+#define HOST_PORT 5000
+#define HOST_PROTOCOL "https"
+#define HOST_BASE_PATH "/receive/update"
 
-AsyncWebServer server(80);
+AsyncWebServer server(SERVER_PORT);
 ESP8266React esp8266React(&server);
-LightMqttSettingsService lightMqttSettingsService =
-    LightMqttSettingsService(&server, esp8266React.getFS(), esp8266React.getSecurityManager());
-LightStateService lightStateService = LightStateService(&server,
-                                                        esp8266React.getSecurityManager(),
-                                                        esp8266React.getMqttClient(),
-                                                        &lightMqttSettingsService);
+RestClient client(HOST_ADDRESS, HOST_PORT, HOST_PROTOCOL, HOST_BASE_PATH);
+Sensor sensor(&client);
 
 void setup() {
-  // start serial and filesystem
   Serial.begin(SERIAL_BAUD_RATE);
-
-  // start the framework and demo project
-  esp8266React.begin();
-
-  // load the initial light settings
-  lightStateService.begin();
-
-  // start the light service
-  lightMqttSettingsService.begin();
-
-  // start the server
+  esp8266React.begin();  // TODO: change demo project
   server.begin();
+  sensor.begin();
 }
 
 void loop() {
-  // run the framework's loop function
   esp8266React.loop();
+  sensor.loop();
 }
